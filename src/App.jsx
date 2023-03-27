3cb9af04f92f2e96cab5d9cf92776123b782689d
luizeduardotesta <luizsonecazzz@hotmail.com>
2023-03-27 13:16:37 -0300

    create menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Cadastro } from "./pages/Cadastro/Cadastro";
+import { Home } from "./pages/Home/Home";
+import { Login } from "./pages/Login/Login";
+import { Root } from "./pages/Root/Root";
+
 export function App() {
-    return <>teste</>;
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Root />}>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/cadastro" element={<Cadastro />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
 }
