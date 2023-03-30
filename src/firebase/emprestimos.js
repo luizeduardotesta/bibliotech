1cfabf604e3edb54093e9d2eaec8e822abd4a0f6
luizeduardotesta <luizsonecazzz@hotmail.com>
2023-03-30 13:07:08 -0300

    add editar emprestimos

--- src/firebase/emprestimos.js
+++ src/firebase/emprestimos.js
@@ -1,5 +1,6 @@
 import { addDoc, getDocs } from "firebase/firestore"
 import { emprestimosCollection } from "./collections";
+import { addDoc, doc, getDoc, getDocs } from "firebase/firestore";
 
 export async function adicionarEmprestimo(data){
     await addDoc(emprestimosCollection, data);
@@ -12,4 +13,9 @@ export async function getEmprestimos(data){
         emprestimos.push({...doc.data(), id: doc.id})
     })
     return emprestimos;
+}
+
+export async function getEmprestimo(id) {
+    const document = await getDoc(doc(emprestimosCollection, id));
+    return {...document.data(), id: document.id};
 }
